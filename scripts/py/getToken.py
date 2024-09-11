6b8edc15959bcd8a17acbfa9c6afbe8bd9ed3ad9
MagPiePL <jsrokowski05@gmail.com>
2024-09-11 18:12:29 +0200

    Waiting for loaded content

--- scripts/py/getToken.py
+++ scripts/py/getToken.py
@@ -1,5 +1,7 @@
 import os
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait  # Import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
 from selenium.webdriver import ChromeService
 from seleniumwire import webdriver
@@ -13,6 +15,14 @@ def login(driver):
     driver.find_element(By.ID, "username").send_keys(email)
     driver.find_element(By.ID, "password").send_keys(password)
     driver.find_element(By.ID, "register-button").click()
+    
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "#main-header-menu > div > div.right-panel.ng-tns-c1251938777-2.ng-star-inserted > div.my-account.ng-tns-c1251938777-2 > div > div > button"))
+        )
+    except:
+        print("Error timeout exception")
+
 
 def main():
     project_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
